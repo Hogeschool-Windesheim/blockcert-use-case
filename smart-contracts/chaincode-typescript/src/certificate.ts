041a6e37557ac51a39a466213d38a9369b57e921
Pumpkindigger <zonneveld.noodrwijk@gmail.com>
2021-03-11 14:29:57 +0100

    Added initial setup for chaincode and application

---
+++ smart-contracts/chaincode-typescript/src/certificate.ts
@@ -0,0 +1,34 @@
+/*
+  SPDX-License-Identifier: Apache-2.0
+*/
+
+import {Object, Property} from 'fabric-contract-api';
+
+
+@Object()
+export class Certificate {
+
+  @Property()
+    public ID: string;
+
+    @Property()
+    public CertNr: string;
+
+    @Property()
+    public StartDate: string;
+
+    @Property()
+    public EndDate: string;
+
+    @Property()
+    public Address: string;
+
+    @Property()
+    public Acquirer: string;
+
+    @Property()
+    public RegistrationNr: string;
+
+    @Property()
+    public State: string;
+}
---
+++ smart-contracts/setup.sh
@@ -0,0 +1,12 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}/../test-network/"
+
+./network.sh down
+./network.sh up createChannel -c mychannel -ca
+
+./network.sh deployCC -ccn basic -ccp ../smart-contracts/chaincode-typescript -ccl typescript
+
+cd "${DIR}/application-typescript/src"
+
+tsc app.ts
+rm -r wallet
