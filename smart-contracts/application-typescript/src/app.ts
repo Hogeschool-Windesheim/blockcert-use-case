c2a84a70fb3f638a1ab2b3aec700d2faffa9d20c
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2021-03-17 14:01:04 +0100

    Reset hardcoded user identifier

--- smart-contracts/application-typescript/src/app.ts
+++ smart-contracts/application-typescript/src/app.ts
@@ -16,7 +16,7 @@ const channelName = 'mychannel';
 const chaincodeName = 'basic';
 const mspOrg1 = 'Org1MSP';
 const walletPath = path.join(__dirname, 'wallet');
-const org1UserId = 'appUser1';
+const org1UserId = 'appUser';
 
 // pre-requisites:
 // - fabric-sample two organization test-network setup with two peers, ordering service,
