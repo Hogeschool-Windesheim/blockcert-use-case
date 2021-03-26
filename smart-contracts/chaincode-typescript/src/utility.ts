39604c52fcaf69fdcbf034f056fccb3f47985efb
Douwe <douwe.hoonhout@systemsnavigator.com>
2021-03-26 12:12:49 +0100

    Fix tslint errors

--- smart-contracts/chaincode-typescript/src/utility.ts
+++ smart-contracts/chaincode-typescript/src/utility.ts
@@ -1,14 +1,19 @@
 import * as moment from 'moment';
+import {State} from './state';
+
+const STATES = new Set([State.ISSUED, State.REVOKED, State.EXPIRED]);
 
 export class Utility {
 
     /**
      * Checks the state of a certificate, throws an error if state is invalid.
-     * @param state the state to be checked
+     * @param stateStr the state to be checked
      */
-    public static checkStateValidity(state: string): void{
-        if (state === 'ISSUED' || state === 'REVOKED' || state === 'EXPIRED'){}
-        else throw new Error('Invalid state')
+    public static checkStateValidity(stateStr: string): void {
+        const state = State[stateStr];
+        if (!STATES.has(state)) {
+            throw new Error('Invalid state');
+        }
     }
 
     /**
@@ -16,12 +21,11 @@ export class Utility {
      * @param dateString the string representation of date
      * @returns Date corresponding to @param dateString
      */
-    public static stringToDate(dateString: string): Date{
+    public static stringToDate(dateString: string): Date {
         try {
             return moment(dateString, 'MM-DD-YYYY').toDate();
         } catch {
-            throw new Error('Invalid date format')
+            throw new Error('Invalid date format');
         }
-        
     }
 }
